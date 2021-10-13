74b3832c901c751772ce081a71cbb6a384f6a33d
LOL211 <kbanbah@yahoo.com>
2021-10-13 17:08:27 +0800

    mocktest

---
+++ software/src/mocktest/LeapYearV1.java
@@ -0,0 +1,11 @@
+package mocktest;
+public class LeapYearV1 {
+	public boolean isLeapYear(int year) {
+		boolean leap;
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+			leap = true;
+		else
+			leap = false;
+		return leap;
+	}
+}
